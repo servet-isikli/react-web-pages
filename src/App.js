dab57d9c4a8199133219d3735a28d7ec41fc9f6e
servet-isikli <servetisikli@yahoo.com>
2023-06-01 11:29:37 +0200

    react-bootstrap is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Home from "./pages/Home";
+import MyNav from "./components/MyNav";
+
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <MyNav />
+      <Home />
     </div>
   );
 }
