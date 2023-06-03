f30221825a1bdb64b3ee229b454d6f214ad6925f
servet-isikli <servetisikli@yahoo.com>
2023-06-03 23:17:36 +0200

    route path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import MyNav from "./components/MyNav";
-
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import FullStack from "./pages/FullStack";
+import Instructors from "./pages/Instructors";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <BrowserRouter>
       <MyNav />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/developing" element={<FullStack />} />
+        <Route path="/data-science" element={<FullStack />} />
+        <Route path="/devops" element={<FullStack />} />
+        <Route path="/instructors" element={<Instructors />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
